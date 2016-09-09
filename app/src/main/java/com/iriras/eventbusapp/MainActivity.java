e4ac8624867610c908acd4b0633c3cba6e30876e
Irfan Irawan Sukirman <irfan.irawan@gits.co.id>
2016-09-09 18:28:24 +0700

    First Commit

---
+++ app/src/main/java/com/iriras/eventbusapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.iriras.eventbusapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
